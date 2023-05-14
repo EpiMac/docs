4994e56ed9050f3073d3c88b9d710024485fc41b
Dov Devers <contact@bugbear.fr>
2023-05-14 23:35:34 +0200

    style: Customized front page to match OSX Ventura

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,34 +8,36 @@ import HomepageTutorials from '@site/src/components/HomepageTutorials';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Get Started
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+    const {siteConfig} = useDocusaurusContext();
+    return (
+        <header className={clsx('hero', styles.heroBanner, styles.heroBackground)}>
+            <div className={clsx("container", styles.heroContainer)}>
+                <h1 className="hero__title">{siteConfig.title}</h1>
+                <p className="hero__subtitle">{siteConfig.tagline}</p>
+                <div className={styles.buttons}>
+                    <Link
+                        className="button button--secondary button--lg"
+                        to="/docs/intro">
+                        Get Started
+                    </Link>
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description="Documentation for EpiMac">
-      <HomepageHeader />
-      <main>
-        <HomepageTutorials />
-      </main>
-    </Layout>
-  );
+    const {siteConfig} = useDocusaurusContext();
+    return (
+        <Layout
+            title={`${siteConfig.title}`}
+            description="Documentation for EpiMac">
+            <div className={styles.logo_banner}>
+                <HomepageHeader/>
+                <main>
+                    <HomepageTutorials/>
+                </main>
+            </div>
+        </Layout>
+    );
 }
