74af49f1b47d98f4674b264d567b4df3db947938
Dov Devers <contact@bugbear.fr>
2023-05-15 10:54:14 +0200

    fix: changed png logos to webp for efficiency

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,8 +62,8 @@ const config = {
                 title: 'Epimac',
                 logo: {
                     alt: 'Epimac Logo',
-                    src: 'img/epimac_black.png',
-                    srcDark: 'img/epimac_white.png',
+                    src: 'img/epimac_black.webp',
+                    srcDark: 'img/epimac_white.webp',
                 },
                 items: [
                     {
